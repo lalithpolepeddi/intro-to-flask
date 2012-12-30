09ce6c43aa01d2c81208540e9a76ae42723ba7df
cuttarug <lpolepeddi@gmail.com>
2012-12-30 17:37:59 -0600

    About page

--- routes.py
+++ routes.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
   return render_template('home.html')
 
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
