1d8150b4e911c2241cc455c7cd85d277c0db83bd
cuttarug <lpolepeddi@gmail.com>
2013-01-03 23:08:56 -0700

    Specific error message flashing

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
 
 class ContactForm(Form):
-  name = TextField("Name",  [validators.Required()])
-  email = TextField("Email",  [validators.Required(), validators.Email()])
-  subject = TextField("Subject",  [validators.Required()])
-  message = TextAreaField("Message",  [validators.Required()])
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
   submit = SubmitField("Send")
