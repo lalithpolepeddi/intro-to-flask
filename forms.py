6074e31de061f58f957a201df4a2728565a2a23c
cuttarug <lpolepeddi@gmail.com>
2013-01-03 22:03:22 -0700

    Contact form validations

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
-from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
 
 class ContactForm(Form):
-  name = TextField("Name")
-  email = TextField("Email")
-  subject = TextField("Subject")
-  message = TextAreaField("Message")
+  name = TextField("Name",  [validators.Required()])
+  email = TextField("Email",  [validators.Required(), validators.Email()])
+  subject = TextField("Subject",  [validators.Required()])
+  message = TextAreaField("Message",  [validators.Required()])
   submit = SubmitField("Send")
