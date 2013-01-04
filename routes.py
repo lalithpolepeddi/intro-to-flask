00597a235cdbfaa7c9b232a16b12e75afbc48207
cuttarug <lpolepeddi@gmail.com>
2013-01-04 13:59:08 -0700

    Email contact form data

--- routes.py
+++ routes.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, flash
 from forms import ContactForm
+from flask.ext.mail import Message, Mail
+
+mail = Mail()
 
 app = Flask(__name__)
 
 app.secret_key = 'development key'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'contact@example.com'
+app.config["MAIL_PASSWORD"] = 'your-password'
+
+mail.init_app(app)
 
 @app.route('/')
 def home():
@@ -16,14 +27,21 @@ def about():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
   form = ContactForm()
-  
+
   if request.method == 'POST':
     if form.validate() == False:
       flash('All fields are required.')
       return render_template('contact.html', form=form)
     else:
+      msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+      msg.body = """
+      From: %s <%s>
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+
       return 'Form posted.'
-  
+
   elif request.method == 'GET':
     return render_template('contact.html', form=form)
   
