523ac6473b082593ff91f741d2fae3eb01b5400d
cuttarug <lpolepeddi@gmail.com>
2013-02-04 02:09:39 -0700

    Profile page

--- intro_to_flask/routes.py
+++ intro_to_flask/routes.py
@@ -1,8 +1,8 @@
 from intro_to_flask import app
-from flask import render_template, request, flash
+from flask import render_template, request, flash, session, url_for, redirect
 from forms import ContactForm, SignupForm
 from flask.ext.mail import Message, Mail
-from models import db
+from models import db, User
 
 mail = Mail()
 
@@ -42,8 +42,26 @@ def signup():
   if request.method == 'POST':
     if form.validate() == False:
       return render_template('signup.html', form=form)
-    else:   
-      return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+    else:
+      newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      
+      session['email'] = newuser.email
+      return redirect(url_for('profile'))
   
   elif request.method == 'GET':
-    return render_template('signup.html', form=form)      
+    return render_template('signup.html', form=form)
+
+@app.route('/profile')
+def profile():
+
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+
+  user = User.query.filter_by(email = session['email']).first()
+
+  if user is None:
+    return redirect(url_for('signin'))
+  else:
+    return render_template('profile.html')    
