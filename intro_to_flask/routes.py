e7f852c0493ca4830938aaae1515c99743496d48
cuttarug <lpolepeddi@gmail.com>
2013-02-04 03:06:10 -0700

    Signout

--- intro_to_flask/routes.py
+++ intro_to_flask/routes.py
@@ -78,4 +78,13 @@ def signin():
       return redirect(url_for('profile'))
                 
   elif request.method == 'GET':
-    return render_template('signin.html', form=form)    
+    return render_template('signin.html', form=form)
+
+@app.route('/signout')
+def signout():
+
+  if 'email' not in session:
+    return redirect(url_for('signin'))
+    
+  session.pop('email', None)
+  return redirect(url_for('home'))
